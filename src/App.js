7d624aca6daad4bf53a6829a23baad07ab8589f9
DhanrajRacharla <Danrajvarma2001@gmail.com>
2025-08-01 12:10:13 +0000

    NodeJs Backend solution

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import BooksList from './components/BooksList'
+
+const App = () => (
+    <div className='bg-container'>
+      <div className='landing-section'>
+        <h1 className='heading'>Good Reads</h1>
+        <p className='quote'>Meet Your Next Favourite Book</p>
+      </div>
+
+      <BooksList />
+    </div>
+  )
+
+export default App
